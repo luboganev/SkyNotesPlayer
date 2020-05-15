17e9e1fccb6c8b35a279f26f523fec838a8857e9
Lyubomir Ganev <luboganev@gmail.com>
2020-05-15 22:48:19 +0200

    Add input param for song name

--- src/main/kotlin/Jukebox.kt
+++ src/main/kotlin/Jukebox.kt
@@ -1,7 +1,7 @@
 /**
  * The beginning of Camila Cabello and Shawn Mendes - Señorita
  */
-val seniorita = Song(
+val senorita = Song(
     songString = "C3****A2****E2****C3B2****" +
             "G2****G2C3B2A2B2C3****G2****E2****C3B2****G2****" +
             "G2C3B2A2G2F2**C3*C3*C3************" +
@@ -9,4 +9,22 @@ val seniorita = Song(
             "B2****C3B2C3B2G2********" +
             "B2****C3B2C3B2G2A2A2",
     singlePauseMillis = 50
+)
+
+/**
+ * Twinkle little star
+ */
+val twinkleStar = Song(
+    songString = "G1*G1*D2*D2*E2*E2*D2********" +
+            "C2*C2*B1*B1*A1*A1*G1********"+
+            "D2*D2*C2*C2*B1*B1*A1********"+
+            "D2*D2*C2*C2*B1*B1*A1********"+
+            "G1*G1*D2*D2*E2*E2*D2********"+
+            "C2*C2*B1*B1*A1*A1*G1",
+    singlePauseMillis = 50
+)
+
+val jukeBox = mapOf<String, Song>(
+    "senorita" to senorita,
+    "twinkle" to twinkleStar
 )
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,24 +4,45 @@ import com.malinskiy.adam.request.devices.ListDevicesRequest
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 
-fun main() {
-    runBlocking {
-        //Verify the ADB server is running
-        StartAdbInteractor().execute()
-        //Create AndroidDebugBridgeServer instance
-        val adb = AndroidDebugBridgeClientFactory().apply {
-            coroutineContext = Dispatchers.IO
-        }.build()
-        //Execute requests using suspendable execute() methods. First list available devices
-        val devices = adb.execute(request = ListDevicesRequest())
-        val serial = devices.first().serial
+fun main(vararg args: String) {
+    val song = if (args.isEmpty()) {
+        println("Available songs:")
+        jukeBox.keys.forEach {
+            println(it)
+        }
+        null
+    } else {
+        val songName = args[0]
+        if (jukeBox[songName] != null) {
+            println("Available songs:")
+            jukeBox.keys.forEach {
+                println(it)
+            }
+            jukeBox[songName]
+        } else {
+            null
+        }
+    }
 
-        // Play a song
-        SongPlayer(
-            adbClient = adb,
-            deviceSerial = serial,
-            keyboard = Keyboard.Pixel2,
-            song = seniorita
-        ).play()
+    if (song != null) {
+        runBlocking {
+            //Verify the ADB server is running
+            StartAdbInteractor().execute()
+            //Create AndroidDebugBridgeServer instance
+            val adb = AndroidDebugBridgeClientFactory().apply {
+                coroutineContext = Dispatchers.IO
+            }.build()
+            //Execute requests using suspendable execute() methods. First list available devices
+            val devices = adb.execute(request = ListDevicesRequest())
+            val serial = devices.first().serial
+
+            // Play a song
+            SongPlayer(
+                adbClient = adb,
+                deviceSerial = serial,
+                keyboard = Keyboard.Pixel2,
+                song = song
+            ).play()
+        }
     }
 }
